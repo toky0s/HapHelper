f81e78f7cc43397f1f81ff37a1c7ad2725827ec4
xinta <truongaxin123@gmail.com>
2023-10-08 11:41:13 +0700

    Create tree

---
+++ HapHelper/HapHtmlNode.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Mvvm;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HapHelper
+{
+    internal class HapHtmlNode: BindableBase
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+        public ObservableCollection<HapHtmlNode> ChildNodes { get; set; }
+        public ObservableCollection<Tuple<string, string>> Attributes { get; set; }
+    }
+}
--- HapHelper/MainWindowVm.cs
+++ HapHelper/MainWindowVm.cs
@@ -1,4 +1,7 @@
-﻿using HapHelper.Services;
+﻿using AngleSharp.Html.Parser;
+using AngleSharp.Html;
+
+using HapHelper.Services;
 
 using HtmlAgilityPack;
 
@@ -7,12 +10,25 @@ using Prism.Mvvm;
 
 using System;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Net.Http;
+
+using AngleSharp.Html.Dom;
+using HAP_HtmlParseError = HtmlAgilityPack.HtmlParseError;
+using System.Windows.Input;
+using AngleSharp.Dom;
 
 namespace HapHelper
 {
     internal class MainWindowVm : BindableBase
     {
+        private Cursor _ui_Cursor;
+        public Cursor UI_Cursor
+        {
+            get { return _ui_Cursor; }
+            set { SetProperty(ref _ui_Cursor, value); }
+        }
+
         private PopupSvc _popupSvc;
         public PopupSvc PopupSvc
         {
@@ -55,18 +71,75 @@ namespace HapHelper
             }
         }
 
-        public ObservableCollection<HtmlParseError> HtmlParseErrors { get; set; }
+        public ObservableCollection<HAP_HtmlParseError> HtmlParseErrors { get; set; }
 
         public DelegateCommand SendCommand { get; private set; }
         public DelegateCommand CheckCommand { get; private set; }
+        public DelegateCommand PrettyCommand { get; private set; }
+        public DelegateCommand RunCommand { get; private set; }
+        #endregion
+
+        #region Document Node
+        private string _documentNodeInnerHtml;
+        public string DocumentNodeInnerHtml
+        {
+            get { return _documentNodeInnerHtml; }
+            set { SetProperty(ref _documentNodeInnerHtml, value); }
+        }
+        #endregion
+
+        #region Pretty View
+        public ObservableCollection<HapHtmlNode> HapNodes { get; set; }
         #endregion
 
         public MainWindowVm()
         {
             PopupSvc = new PopupSvc();
-            HtmlParseErrors = new ObservableCollection<HtmlParseError>();
+            HtmlParseErrors = new ObservableCollection<HAP_HtmlParseError>();
+            HapNodes = new ObservableCollection<HapHtmlNode>();
             SendCommand = new DelegateCommand(SendHttpRequestGet, CanSend).ObservesProperty(() => HtmlSourceUrl);
             CheckCommand = new DelegateCommand(Check, CanCheck).ObservesProperty(() => MyRawHtml);
+            PrettyCommand = new DelegateCommand(Pretty, CanCheck).ObservesProperty(() => MyRawHtml);
+            RunCommand = new DelegateCommand(Run, CanRun).ObservesProperty(() => MyRawHtml).ObservesProperty(() => IsRawHtmlValid);
+
+            UI_Cursor = Cursors.Arrow;
+        }
+
+        private bool CanRun()
+        {
+            return MyRawHtml != null 
+                && MyRawHtml.Trim().Length > 0 
+                && IsRawHtmlValid;
+        }
+
+        private void Run()
+        {
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(MyRawHtml);
+            DocumentNodeInnerHtml = htmlDocument.DocumentNode.InnerHtml;
+            HapNodes.Add(GetHapHtmlNode(htmlDocument.DocumentNode));
+        }
+
+        private HapHtmlNode GetHapHtmlNode(HtmlNode htmlNode)
+        {
+            HapHtmlNode hapHtmlNode = new HapHtmlNode
+            {
+                Name = htmlNode.Name,
+                Attributes = new ObservableCollection<Tuple<string, string>>(),
+                ChildNodes = new ObservableCollection<HapHtmlNode>(),
+            };
+            foreach (var attr in htmlNode.Attributes)
+            {
+                Tuple<string, string> hapAttr = Tuple.Create(attr.Name, attr.Value);
+                hapHtmlNode.Attributes.Add(hapAttr);
+            }
+            foreach (var node in htmlNode.ChildNodes)
+            {
+
+            hapHtmlNode.ChildNodes.Add(GetHapHtmlNode(node));
+
+            }
+            return hapHtmlNode;
         }
 
         private async void SendHttpRequestGet()
@@ -99,6 +172,23 @@ namespace HapHelper
             //Check();
         }
 
+        private void Pretty()
+        {
+            UI_Wait();
+            HtmlParser parser = new HtmlParser();
+            IHtmlDocument document = parser.ParseDocument(MyRawHtml);
+            using (StringWriter writer = new StringWriter())
+            {
+                document.ToHtml(writer, new PrettyMarkupFormatter
+                {
+                    Indentation = "  ",
+                    NewLine = "\n"
+                });
+                MyRawHtml = writer.ToString();
+            }
+            UI_RemoveWait();
+        }
+
         private bool CanSend()
         {
             return !(_htmlSourceUrl is null)
@@ -110,7 +200,7 @@ namespace HapHelper
             HtmlParseErrors.Clear();
             HtmlDocument document = new HtmlDocument();
             document.LoadHtml(MyRawHtml);
-            foreach (HtmlParseError htmlParseError in document.ParseErrors)
+            foreach (HAP_HtmlParseError htmlParseError in document.ParseErrors)
             {
                 HtmlParseErrors.Add(htmlParseError);
             }
@@ -124,6 +214,16 @@ namespace HapHelper
         {
             return !(MyRawHtml is null) 
                 && MyRawHtml.Trim().Length > 0;
+        }
+
+        private void UI_Wait()
+        {
+            UI_Cursor = Cursors.Wait;
+        }
+
+        private void UI_RemoveWait()
+        {
+            UI_Cursor = Cursors.Arrow;
         }
     }
 }
